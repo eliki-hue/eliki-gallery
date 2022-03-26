9b515b83701aecd720e1ef70dfee90593e206999
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-26 12:18:55 +0300

    installed django-bootstrap5

--- myGallery/tests.py
+++ myGallery/tests.py
@@ -9,8 +9,27 @@ from models import Location, Category, Image
 class ImageTestClass(TestCase):
 
     def setUp(self):
-        self.image1=Image(image='image1.png',image_name="image1",image_description='test image',image_location='egerton',image_category='school')
+        
+        self.new_location= Location(name = 'test_location')
+        self.new_location.save()
+
+        self.new_category= Category(name = 'test_category')
+        self.new_category.save()
 
 
-    def test_save_image(self):
-        self.image1.save_image()
+        self.image1=Image(image='image1.png',image_name="image1",image_description='test image', image_lacation="test_location",image_category="test_category")
+
+        self.image1.save()
+
+    #  # Creating a new tag and saving it
+    #     self.new_location= Location(name = 'test_location')
+    #     self.new_location.save()
+
+    #     self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+    #     self.new_article.save()
+
+    #     self.new_article.tags.add(self.new_tag)
+
+    # def tearDown(self):
+    #     Editor.objects.all().delete()
+    #     tags.objects.all().delete()
--- myGallery/views.py
+++ myGallery/views.py
@@ -1,7 +1,9 @@
+from tkinter import Image
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 # Create your views here.
 
 def home(request):
     welcome = 'Welcome to my image Gallery'
+    images= Image.objects.get()
     return render (request,'home.html',{'message': welcome})
