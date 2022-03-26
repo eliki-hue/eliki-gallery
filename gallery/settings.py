8da591cd33dfe1f79bd10295000eadbccfad4993
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-26 15:37:58 +0300

    changed from bootstrap5 > bootstrap3

--- gallery/settings.py
+++ gallery/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myGallery.apps.MygalleryConfig',
-    'bootstrap5'
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
