d9ee88c38f8c87b7eb356843da9cf1740fd86cdd
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-28 00:42:23 +0300

    addedd id and location serch views

--- myGallery/views.py
+++ myGallery/views.py
@@ -18,39 +18,28 @@ def search_results(request):
 
     if 'image_category' in request.GET and request.GET["image_category"]:
         search_term = request.GET.get("image_category")
-        searched_category = Image.objects.filter(id=search_term)
-        message = f"{search_term}"
-        print(search_term)
-        print(searched_category)
-        return render(request, 'search.html',{"message":message,"images": searched_category})
-
-    else:
-        message = "You haven't searched for any category"
-        return render(request, 'search.html',{"message":message})
-
-def search_results(request):
-
-    if 'image_category' in request.GET and request.GET["image_category"]:
-        search_term = request.GET.get("image_category")
         searched_category = Image.search_by_category(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"images": searched_category})
 
+    elif 'image_id' in request.GET and request.GET["image_id"]:
+        search_term = request.GET.get("image_id")
+        searched_category = Image.get_image_by_id(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_category})
+
+    elif 'image_location' in request.GET and request.GET["image_location"]:
+        search_term = request.GET.get("image_location")
+        searched_category = Image.filter_by_location(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_category})
+
+
+
     else:
         message = "You haven't searched for any category"
         return render(request, 'search.html',{"message":message})
 
-
-# def search_results(request):
-
-#     if 'image_category' in request.GET and request.GET["image_category"]:
-#         search_term = request.GET.get("image_category")
-#         searched_category = Image.search_by_category(search_term)
-#         message = f"{search_term}"
-
-#         return render(request, 'search.html',{"message":message,"images": searched_category})
-
-#     else:
-#         message = "You haven't searched for any category"
-        return render(request, 'search.html',{"message":message})
